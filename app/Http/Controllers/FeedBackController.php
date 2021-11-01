f4ec4437478d3aee7e21d90584418466b46c9310
Aryas1378 <aryas1378@gmail.com>
2021-11-01 10:07:01 +0330

    store method is modified in feedback controller

--- app/Http/Controllers/FeedBackController.php
+++ app/Http/Controllers/FeedBackController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\FeedBackStoreRequest;
+use App\Http\Requests\MenuItemStoreRequest;
+use App\Http\Resources\FeedBackResource;
 use App\Models\MenuItem;
 use Illuminate\Http\Request;
 
 class FeedBackController extends Controller
 {
-
+    public function store(FeedBackStoreRequest $request, MenuItem $menuItem)
+    {
+        $feedBack = $menuItem->feedbacks()->create([
+            'description' => $request->get('description')
+        ]);
+        return $this->success(new FeedBackResource($feedBack));
+    }
 }
