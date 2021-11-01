3cafd0a87404ebb478bf7c12ea2f83621d48f535
Aryas1378 <aryas1378@gmail.com>
2021-11-01 09:02:10 +0330

    menu_item_id column is added to feedbacks table

---
+++ database/migrations/2021_11_01_052922_add_menu_item_id_column_to_feedbacks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMenuItemIdColumnToFeedbacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('feedbacks', function (Blueprint $table) {
+            $table->foreignId('menu_item_id')->constrained('menu_items')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('feedbacks', function (Blueprint $table) {
+            //
+        });
+    }
+}
